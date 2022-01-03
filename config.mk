7d5c0653d828ac983074a2b1607b43741bea2fa0
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-01-03 18:18:32 +0530

    config: update default sounds to SD1A
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- config.mk
+++ config.mk
@@ -24,6 +24,6 @@ PRODUCT_PACKAGES += \
 
 # Set default ringtone, notification and alarm
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.alarm_alert=Bright_morning.ogg \
-    ro.config.ringtone=The_big_adventure.ogg \
-    ro.config.notification_sound=Popcorn.ogg
+    ro.config.ringtone=Your_new_adventure.ogg \
+    ro.config.notification_sound=Eureka.ogg \
+    ro.config.alarm_alert=Fresh_start.ogg
