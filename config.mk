f9ad868dc3d3386c569f49dfb511f7d621d3d062
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-12 20:31:12 +0900

    pixel: update to redfin-user 12 SP2A.220305.012 8177914 release-keys
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- config.mk
+++ config.mk
@@ -24,9 +24,9 @@ PRODUCT_PACKAGES += \
 
 # Set default ringtone, notification and alarm
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Your_new_adventure.ogg \
-    ro.config.notification_sound=Eureka.ogg \
-    ro.config.alarm_alert=Fresh_start.ogg
+    ro.config.alarm_alert=Bright_morning.ogg \
+    ro.config.notification_sound=Popcorn.ogg \
+    ro.config.ringtone=The_big_adventure.ogg
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     vendor/google/pixel/sepolicy
